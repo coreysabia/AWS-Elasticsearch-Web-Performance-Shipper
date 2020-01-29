ffab9d97e2582ef45ada6247542eb20896a5688e
Corey Sabia <12410796+coreysabia@users.noreply.github.com>
2020-01-29 10:26:51 -0500

    first test file

---
+++ src/test.py
@@ -0,0 +1,36 @@
+import datetime as dt
+import sys
+import os
+
+from seleniumDataShipper import SeleniumDataShipper
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+import config
+
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+def main():
+
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    options.add_argument('window-size=1200x600')
+    options.add_argument('--no-sandbox')
+
+    driver = webdriver.Chrome(options=options)
+
+    webpage = "https://www.google.com"
+
+    print("Loading: ", webpage)
+    print()
+    driver.get(webpage)
+    data = driver.execute_script("return window.performance.timing.toJSON();")
+    print("Collecting data:", data)
+    print()
+    print("Closing driver.")
+    driver.quit()
+
+
+
+if __name__ == "__main__":
+    main()
